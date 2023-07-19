e63d89541c10f412aad5792d97c3384b4e4b3a75
mghazidev <mghazidev@gmail.com>
2023-07-19 18:06:08 +0500

    React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "./name";
+
+
+function App() {
+  return (
+    <Name>
+      <h1>Hello my name is ghazi</h1>
+    </Name>
+  )
+}
+
+export default App;
---
+++ src/name.js
@@ -0,0 +1,5 @@
+function Name({child}) {
+    return <div>{child}</div>;
+  }
+
+  export default Name;
