548e129e83ec99751ed430e95935990a7c1bd822
mghazidev <mghazidev@gmail.com>
2023-07-20 03:19:24 +0500

    API

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import Search from "./Search";
 import Pagination from "./Pagination";
 import  Story  from "./Story";
-import { useGLobalContext } from "./context";
 
 const App = () => {
-    const data = useGLobalContext();
     return(
         <>
-        <div> Hello {data}</div>
+        <div> Hello</div>
         <Search />
         <Pagination />
         <Story />   
--- api/src/Story.jsx
+++ api/src/Story.jsx
@@ -1,35 +1,38 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import { useGLobalContext } from './context';
 
 const Story = () => {
-    let isLoading = true;
-    let API = "http://hn.algolia.com/api/v1/search?query=html";
-
-    const fetchApiData = async(url) => {
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            console.log(data);
-        } catch(error) {
-            console.log(error);
-        }
-    }
-    
-    
-    useEffect(() => {
-      fetchApiData(API)
-    
-    }, []);
-    
-    if (isLoading) {
+    const {hits, isLoading } = useGLobalContext()  
+    if(isLoading) {
         return (
             <>
-                <h1>...Load</h1>
+            <h1>Loading....</h1>
             </>
         )
-    }
+    } 
   return (
+    <>
     <h2>My tech</h2>
-  )
+    {hits.map((action) => {
+        const { title , author, objectID, url, num_comments } = action;
+        return <>
+        <div className="card">
+            <h2>{title}</h2>
+            <p> 
+                by <span>{author}</span> | <span>{num_comments}</span> comments
+            </p>
+
+            <div className="card-btn">
+                <a href='{url}' >
+                    Read mOre
+                </a>
+                <a href='#'>REmove</a>
+            </div>
+        </div>
+        </>
+     } )}
+    </>
+ )
 }
 
 export default Story
